2c39231bd1d486cedafa6f8149b52d0297c9e1d9
ypap1234pun <yaperera2@gmail.com>
2024-10-31 11:15:30 +0530

    tulple introduce

--- student.py
+++ student.py
@@ -1,13 +1,10 @@
 def main():
-    name = get_name()
-    house = get_house()
-    print(f"{name} from {house}")
+    student= get_student()
+    print(f"{student[0] from {house[1]}")
 
-def get_name():
-    return input("Name:")
-    
-def get_house():
-    return input("House:")
-
+def get_student():
+    name= input("Name:")
+    house= input("House:")
+    return (name,house)
 
 main()
